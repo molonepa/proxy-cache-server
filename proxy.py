c70dceb3481493a11cac30e6664a94f51fafcb9d
Paolo Moloney <molonepa@tcd.ie>
2019-02-06 10:45:04 +0000

    Implement listening socket on server

--- proxy.py
+++ proxy.py
@@ -1,2 +1,22 @@
 import socket
-import Thread
+import _thread
+
+proxy_addr = '127.0.0.1'
+port = 8080
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((proxy_addr, port))
+    while True:
+        # listen forever for incoming connections
+        s.listen()
+        client_to_proxy, addr = s.accept()
+        with client_to_proxy:
+            print('connected by', addr)
+            data = client_to_proxy.recv(1024)
+            if not data:
+                # if no data received, loop again
+                break
+            else:
+                data = data.decode()
+                print('received ', data)
+
