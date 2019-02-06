22e00bbe5a8997c71eab5169d2aee10fd32d8dc5
Paolo Moloney <molonepa@tcd.ie>
2019-02-06 12:53:23 +0000

    Implement threading of socket processes

--- proxy.py
+++ proxy.py
@@ -1,22 +1,46 @@
 import socket
-import _thread
+import threading
+import sys
 
-proxy_addr = '127.0.0.1'
-port = 8080
+MAX_CHUNK_SIZE = 1024
+PROXY_ADDR = '127.0.0.1'
+PORT = 8080
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((proxy_addr, port))
+num_threads = int(sys.argv[1])
+
+class c2p_thread(threading.Thread):
+    # method for thread setup
+    def __init__(self, threadID, addr, port):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.addr = addr
+        self.port = port
+    # method for thread execution
+    def run(self):
+        print('starting thread', self.threadID)
+        client_to_proxy(self.threadID, self.addr, self.port)
+        print('exiting thread', self.threadID)
+
+def client_to_proxy(threadID, addr, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((addr, port))
     while True:
-        # listen forever for incoming connections
         s.listen()
-        client_to_proxy, addr = s.accept()
-        with client_to_proxy:
-            print('connected by', addr)
-            data = client_to_proxy.recv(1024)
+        conn, conn_addr = s.accept()
+        with conn:
+            print('connected by', conn_addr)
+            data = conn.recv(MAX_CHUNK_SIZE)
             if not data:
-                # if no data received, loop again
                 break
             else:
-                data = data.decode()
-                print('received ', data)
+                data_str = data.decode()
+                print('received:', data)
 
+try:
+    for t in range(num_threads):
+        thread = c2p_thread(t, PROXY_ADDR, (PORT+t))
+        thread.start()
+        thread.join()
+        break
+except:
+    print('error: unable to start thread(s)')
