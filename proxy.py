f9ef7a8033973e60fd194ad166a73b0a4f0891d3
Paolo Moloney <molonepa@tcd.ie>
2019-02-11 11:34:53 +0000

    Implement HTTP request parsing

--- proxy.py
+++ proxy.py
@@ -1,46 +1,61 @@
+import sys, io
 import socket
 import threading
-import sys
+from http.server import BaseHTTPRequestHandler
 
 MAX_CHUNK_SIZE = 1024
 PROXY_ADDR = '127.0.0.1'
 PORT = 8080
 
-num_threads = int(sys.argv[1])
-
 class c2p_thread(threading.Thread):
-    # method for thread setup
+    # thread setup
     def __init__(self, threadID, addr, port):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.addr = addr
         self.port = port
-    # method for thread execution
+        self.setDaemon(True)
+    # thread execution
     def run(self):
         print('starting thread', self.threadID)
         client_to_proxy(self.threadID, self.addr, self.port)
         print('exiting thread', self.threadID)
 
+class HTTPRequest(BaseHTTPRequestHandler):
+    # HTTP request parsing
+    def __init__(self, request):
+        self.rfile = io.StringIO(request)
+        self.raw_requestline = self.rfile.readline()
+        self.error_code = self.error_message = None
+        self.parse_request()
+    # HTTP error handling
+    def send_error(self, code, message):
+        self.error_code = code
+        self.error_message = message
+
 def client_to_proxy(threadID, addr, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((addr, port))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((addr, port))
     while True:
-        s.listen()
-        conn, conn_addr = s.accept()
+        sock.listen()
+        conn, conn_addr = sock.accept()
         with conn:
             print('connected by', conn_addr)
-            data = conn.recv(MAX_CHUNK_SIZE)
-            if not data:
+            request = conn.recv(MAX_CHUNK_SIZE)
+            if not request:
                 break
             else:
-                data_str = data.decode()
-                print('received:', data)
+                request = request.decode()
+                request = HTTPRequest(request)
+                print(request.error_code)
+                print(request.command)
+                print(request.path)
+                print(request.request_version)
+                print(len(request.headers))
+                print(request.headers.keys())
+                print(request.headers['host'])
 
-try:
-    for t in range(num_threads):
-        thread = c2p_thread(t, PROXY_ADDR, (PORT+t))
-        thread.start()
-        thread.join()
-        break
-except:
-    print('error: unable to start thread(s)')
+thread = c2p_thread(1, PROXY_ADDR, PORT)
+thread.start()
+thread.join()
