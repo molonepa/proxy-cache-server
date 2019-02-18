41c8b0d5f12faa93265e3ba36352aeb0c455f64b
Paolo Moloney <molonepa@tcd.ie>
2019-02-18 12:07:50 +0000

    Implement request class and parsing

--- proxy.py
+++ proxy.py
@@ -1,13 +1,15 @@
-import sys, io
+import sys
 import socket
 import threading
-from http.server import BaseHTTPRequestHandler
 
 MAX_CHUNK_SIZE = 1024
 PROXY_ADDR = '127.0.0.1'
-PORT = 8080
+PORT = 8080 #default port
 
-class c2p_thread(threading.Thread):
+"""
+Thread class that deals with a connection from a client and processes it then sends it to the specified webserver
+"""
+class connThread(threading.Thread):
     # thread setup
     def __init__(self, threadID, addr, port):
         threading.Thread.__init__(self)
@@ -18,22 +20,43 @@ class c2p_thread(threading.Thread):
     # thread execution
     def run(self):
         print('starting thread', self.threadID)
-        client_to_proxy(self.threadID, self.addr, self.port)
+        clientToProxy(self.threadID, self.addr, self.port)
         print('exiting thread', self.threadID)
 
-class HTTPRequest(BaseHTTPRequestHandler):
-    # HTTP request parsing
-    def __init__(self, request):
-        self.rfile = io.StringIO(request)
-        self.raw_requestline = self.rfile.readline()
-        self.error_code = self.error_message = None
-        self.parse_request()
-    # HTTP error handling
-    def send_error(self, code, message):
-        self.error_code = code
-        self.error_message = message
+"""
+Class is for parsing HTTP requests into their components
+"""
+class request():
+    # set initial values
+    def __init__(self, request_str):
+        self.request_str = request_str
+        self.webserver = ""
+        self.port = -1
+    # parse request
+    def parse(self):
+        first = self.request_str.split('\n')[0]
+        url = first.split(' ')[1]
+    
+        http_pos = url.find("://")
+        if (http_pos == -1):
+            temp = url
+        else:
+            temp = url[(http_pos + 3):]
+    
+        port_pos = temp.find(":")
 
-def client_to_proxy(threadID, addr, port):
+        webserver_pos = temp.find("/")
+        if (webserver_pos == -1):
+            webserver_pos = len(temp)
+
+        if (port_pos == -1 or webserver_pos < port_pos):
+            self.port = PORT
+            self.webserver = temp[:webserver_pos]
+        else:
+            port = int((temp[(port_pos + 1):])[:webserver_pos - port_pos - 1])
+            self.webserver = temp[:port_pos]
+
+def clientToProxy(threadID, addr, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((addr, port))
@@ -42,20 +65,23 @@ def client_to_proxy(threadID, addr, port):
         conn, conn_addr = sock.accept()
         with conn:
             print('connected by', conn_addr)
-            request = conn.recv(MAX_CHUNK_SIZE)
-            if not request:
+            data = conn.recv(MAX_CHUNK_SIZE)
+            if not data:
                 break
             else:
-                request = request.decode()
-                request = HTTPRequest(request)
-                print(request.error_code)
-                print(request.command)
-                print(request.path)
-                print(request.request_version)
-                print(len(request.headers))
-                print(request.headers.keys())
-                print(request.headers['host'])
+                request_str = data.decode()
+                print(request_str)
+                req = request(request_str)
+                req.parse()
+                print(req.webserver)
+                print(req.port)
 
-thread = c2p_thread(1, PROXY_ADDR, PORT)
+def proxyToServer(threadID, addr, port, request):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.connect((addr, port))
+    sock.sendall(request)
+
+thread = connThread(1, PROXY_ADDR, PORT)
 thread.start()
 thread.join()
